105503dfac445ee549df768f3f474f7cae093672
Nuist666 <yzwangkaicheng@gmail.com>
2024-04-13 08:32:24 +0800

    Initial commit

---
+++ 4.3.1 创建数据框[47].py
@@ -0,0 +1,12 @@
+# 抽取其中的序列对象
+import pandas as pd
+
+a = pd.DataFrame([[21.7, 983, 0.64],
+                  [19.2, 991, 0.75],
+                  [13.4, 973, 0.83]],
+                 index=['s1', 's2', 's3'],
+                 columns=['t', 'p', 'rh']
+                 )
+b = a['t']
+print(b)
+print(type(b))
